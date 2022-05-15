6dbe836bc6e6d60dfd09da9496d6f3ff2103297d
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-05-15 16:15:31 -0300

    passando parâmetros por rota até veiculos

--- src/Pages/Vehicles/index.js
+++ src/Pages/Vehicles/index.js
@@ -13,42 +13,74 @@ import Button from "../../Components/Button";
 import Checkbox from "../../Components/Checkbox";
 import AddItemModal from "../../Components/AddItemModal";
 
-export default function Vehicles({ navigation }) {
-  const [initialList, setInitialList] = useState(
-    [
-        {
-        id: 1,
-        title: "A pé",
-        },
-        {
-        id: 2,
-        title: "Bicicleta",
-        },
-        {
-        id: 3,
-        title: "Bicicleta elétrica",
-        },
-        {
-        id: 4,
-        title: "Bicicleta motorizada",
-        },
-        {
-        id: 5,
-        title: "Moto",
-        },
-        {
-        id: 6,
-        title: "Carro",
-        },
-        {
-        id: 7,
-        title: "Patinete",
-        },
-    ]
-    );
+export default function Vehicles({ navigation, route }) {
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    gender,
+    birthDate,
+    state,
+    city,
+    neighborhood,
+  } = route.params;
+  const [vehicles, setVehicles] = useState([
+    {
+      id: 1,
+      title: "A pé",
+    },
+    {
+      id: 2,
+      title: "Bicicleta",
+    },
+    {
+      id: 3,
+      title: "Bicicleta elétrica",
+    },
+    {
+      id: 4,
+      title: "Bicicleta motorizada",
+    },
+    {
+      id: 5,
+      title: "Moto",
+    },
+    {
+      id: 6,
+      title: "Carro",
+    },
+    {
+      id: 7,
+      title: "Patinete",
+    },
+  ]);
+
+  function nextScreen() {
+    if (!vehicles) {
+      showMessage({
+        message: "Falta preencher a lista de veículos!",
+        type: "danger",
+        icon: "danger",
+      });
+    } else {
+      navigation.navigate("Plataformas", {
+        email,
+        password,
+        firstName,
+        lastName,
+        gender,
+        birthDate,
+        state,
+        city,
+        neighborhood,
+        vehicles,
+      });
+    }
+  }
 
   const [checkboxes, setCheckboxes] = useState(initialList);
-  
+
   const Item = ({ item }) => <Checkbox marginTop={30} label={item.title} />;
 
   const renderItem = ({ item }) => <Item item={item} />;
@@ -70,9 +102,9 @@ export default function Vehicles({ navigation }) {
         />
         <AddItemModal
           label="Adicionar novo veículo"
-          initialList = {initialList}
+          initialList={initialList}
           setInitialList={setInitialList}
-          checkboxes = {checkboxes}
+          checkboxes={checkboxes}
           setCheckboxes={setCheckboxes}
         />
         <ButtonContainer>
@@ -83,7 +115,7 @@ export default function Vehicles({ navigation }) {
             marginLeft={0}
             background={"white"}
             size={18}
-            onPress={() => navigation.navigate("Plataformas")}
+            onPress={nextScreen}
           />
         </ButtonContainer>
       </Container>
--- src/Pages/WhereYouLive/index.js
+++ src/Pages/WhereYouLive/index.js
@@ -1,98 +1,115 @@
-import {useState} from 'react';
+import React, { useState } from "react";
 
-import {
-  Container,
-  TitleContainer,
-  Text,
-  ButtonContainer,
-  
-} from './styles';
-import SafeArea from '../../Utils/SafeArea';
-import Button from '../../Components/Button';
-import Dropdown from '../../Components/Dropdown';
+import { Container, TitleContainer, Text, ButtonContainer } from "./styles";
+import SafeArea from "../../Utils/SafeArea";
+import Button from "../../Components/Button";
+import Dropdown from "../../Components/Dropdown";
 
+const WhereYouLive = ({ navigation, route }) => {
+  const { email, password, firstName, lastName, gender, birthDate } =
+    route.params;
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [neighborhood, setNeighborhood] = useState("");
 
-
-const WhereYouLive= ({navigation}) => {
-    const [email,onChangeEmail]=useState('')
-    const [password,onChangePassword]=useState('')
-    
-    const dataState = [
-      {
-        id: '1',
-        value:'rj',
-        label: 'RJ',
-      },
-      {
-        id: '2',
-        value:'sp',
-        label: 'SP',
-      }
-    ];
-
-    const dataCity = [
-        {
-          id: '1',
-          value:'rio',
-          label: 'Rio de Janeiro',
-        },
-        {
-          id: '2',
-          value:'saoPaulo',
-          label: 'São Paulo',
-        }
-      ];
-
-      const dataNeighborhood = [
-        {
-          id: '1',
-          value:'botafogo',
-          label: 'Botafogo',
-        },
-        {
-          id: '2',
-          value:'morumbi',
-          label: 'Morumbi',
-        },
-        {
-            id: '3',
-            value:'copa',
-            label: 'Copacabana',
-        },
-        {
-            id: '4',
-            value:'fla',
-            label: 'Flamengo',
-        },
-        
-      ];
-    
-    return (
-      <SafeArea>
-        <Container>
-            <TitleContainer>
-              <Text title={true}>Onde você mora</Text>
-              <Text >Queremos entender onde você trabalha para poder oferecer 
-                  opções especiais dos{'\n'} entregadores da sua região.
-              </Text>
-            </TitleContainer>
-            <Dropdown label="Estado"  data={dataState}  marginTop={30}/>
-            <Dropdown label="Cidade"  data={dataCity}  marginTop={30}/>
-            <Dropdown label="Bairro"  data={dataNeighborhood}  marginTop={30}/>
-            <ButtonContainer>
-              <Button
-                width={'90%'}
-                text='Próximo'
-                marginTop={350}
-                marginLeft={0}
-                background={'white'}
-                size={18}
-                onPress={() => navigation.navigate('Veiculos')}
-              />
-            </ButtonContainer>
-        </Container>         
-      </SafeArea>
-    );
+  function nextScreen() {
+    if (!state || !city || !neighborhood) {
+      showMessage({
+        message: "Falta preencher a sua residência!",
+        type: "danger",
+        icon: "danger",
+      });
+    } else {
+      navigation.navigate("Veiculos", {
+        email,
+        password,
+        firstName,
+        lastName,
+        gender,
+        birthDate,
+        state,
+        city,
+        neighborhood,
+      });
+    }
   }
 
-    export default WhereYouLive;
+  const dataState = [
+    {
+      id: "1",
+      value: "rj",
+      label: "RJ",
+    },
+    {
+      id: "2",
+      value: "sp",
+      label: "SP",
+    },
+  ];
+
+  const dataCity = [
+    {
+      id: "1",
+      value: "rio",
+      label: "Rio de Janeiro",
+    },
+    {
+      id: "2",
+      value: "saoPaulo",
+      label: "São Paulo",
+    },
+  ];
+
+  const dataNeighborhood = [
+    {
+      id: "1",
+      value: "botafogo",
+      label: "Botafogo",
+    },
+    {
+      id: "2",
+      value: "morumbi",
+      label: "Morumbi",
+    },
+    {
+      id: "3",
+      value: "copa",
+      label: "Copacabana",
+    },
+    {
+      id: "4",
+      value: "fla",
+      label: "Flamengo",
+    },
+  ];
+
+  return (
+    <SafeArea>
+      <Container>
+        <TitleContainer>
+          <Text title={true}>Onde você mora</Text>
+          <Text>
+            Queremos entender onde você trabalha para poder oferecer opções
+            especiais dos{"\n"} entregadores da sua região.
+          </Text>
+        </TitleContainer>
+        <Dropdown label="Estado" data={dataState} marginTop={30} />
+        <Dropdown label="Cidade" data={dataCity} marginTop={30} />
+        <Dropdown label="Bairro" data={dataNeighborhood} marginTop={30} />
+        <ButtonContainer>
+          <Button
+            width={"90%"}
+            text="Próximo"
+            marginTop={350}
+            marginLeft={0}
+            background={"white"}
+            size={18}
+            onPress={nextScreen}
+          />
+        </ButtonContainer>
+      </Container>
+    </SafeArea>
+  );
+};
+
+export default WhereYouLive;
